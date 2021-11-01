43fbf8e1043c3f442dfdb847d36d4fae207fbaa4
DvOmar76 <84674856+DvOmar76@users.noreply.github.com>
2021-11-01 19:38:32 +0300

    Initial commit

---
+++ app/src/main/java/com/example/recipeappfirebaseoptional/Book.kt
@@ -0,0 +1,9 @@
+package com.example.recipeappfirebaseoptional
+data class Book(
+   val id: String,
+   val title: String,
+   val author: String,
+   val ingredients: String,
+   val instructions: String
+
+   )
